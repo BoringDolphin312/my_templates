e7a5ec02ced06e973f19d78085178adaf2bf3752
patrick.makimoto <patrick.makimoto@fpf.br>
2024-03-20 08:52:12 -0400

    adding python

---
+++ python/pyls_config.py
@@ -0,0 +1,19 @@
+config = {
+    "plugins": {
+        "jedi_completion": {"enabled": True},
+        "jedi_definition": {"enabled": True},
+        "jedi_hover": {"enabled": True},
+        "jedi_references": {"enabled": True},
+        "jedi_signature_help": {"enabled": True},
+        "jedi_symbols": {"enabled": True},
+        "jedi_workspace_symbols": {"enabled": True},
+    },
+    "jedi": {
+        "extra_sys_path": [
+            "/home/patrick/Documents/PersonalProject/my_templates/python/"
+        ],
+        "environment": {
+            "PYTHONPATH": "/home/patrick/Documents/FPF-projetos/screwing/screwing-core/src"
+        }
+    }
+}
